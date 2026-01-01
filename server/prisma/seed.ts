37144847dc05943929e66ae017f7841ba5a3e239
Ricardo Altmann <tenaltmann@gmail.com>
2026-01-01 20:37:59 -0300

    Remoção dos habitos eventualmente criados no banco de dados, para exibição apenas do conteudo do seed.ts com o deleteMany

--- server/prisma/seed.ts
+++ server/prisma/seed.ts
@@ -2,7 +2,10 @@ import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
 async function main() {
-    await prisma.habit.create({
+
+    await prisma.habit.deleteMany()         //Deleta os hábitos eventualmente que criados para que por padrão seja mostrado apenas o conteudo do seed.ts
+
+    await prisma.habit.create({             // Cria o habito que aparecera para teste no banco de dados
         data: {
             title:'Beber 2L de água',
             created_at: new Date('2026-01-10T00:00:00.000z')
