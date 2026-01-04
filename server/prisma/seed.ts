e064b2e0f0eae7700d875861b82534332d3cffe3
Ricardo Altmann <tenaltmann@gmail.com>
2026-01-03 23:08:12 -0300

    criação dos segundo e terceiro hábitos

--- server/prisma/seed.ts
+++ server/prisma/seed.ts
@@ -1,5 +1,17 @@
 import { PrismaClient } from '@prisma/client'
+
 const prisma = new PrismaClient()
+
+
+const firstHabitId = '0730ffac-d039-4194-9571-01aa2aa0efbd'
+const firstHabitCreationDate = new Date('2026-01-03T03:00:00.000')
+
+const secondHabitId = '00880d75-a933-4fef-94ab-e05744435297'
+const secondHabitCreationDate = new Date('2026-01-04T03:00:00.000')
+
+
+const thirdHabitId = 'fa1a1bcf-3d87-4626-8c0d-d7fd1255ac00'
+const thirdHabitCreationDate = new Date('2026-01-08T03:00:00.000')
 
 async function main() {
 
