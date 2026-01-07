65cabdc47d04242d59aafb799901a6adeeac3ea1
Ricardo Altmann <tenaltmann@gmail.com>
2026-01-06 22:15:14 -0300

    Cração da conclusão dos habitos pré-criados no seed

--- server/prisma/seed.ts
+++ server/prisma/seed.ts
@@ -4,14 +4,14 @@ const prisma = new PrismaClient()
 
 
 const firstHabitId = '0730ffac-d039-4194-9571-01aa2aa0efbd'
-const firstHabitCreationDate = new Date('2026-01-03T03:00:00.000')
+const firstHabitCreationDate = new Date('2022-12-31T03:00:00.000')
 
 const secondHabitId = '00880d75-a933-4fef-94ab-e05744435297'
-const secondHabitCreationDate = new Date('2026-01-04T03:00:00.000')
+const secondHabitCreationDate = new Date('2023-01-03T03:00:00.000')
 
 
 const thirdHabitId = 'fa1a1bcf-3d87-4626-8c0d-d7fd1255ac00'
-const thirdHabitCreationDate = new Date('2026-01-08T03:00:00.000')
+const thirdHabitCreationDate = new Date('2023-01-08T03:00:00.000')
 
 async function run() {
 
@@ -86,7 +86,8 @@ async function run() {
          */
         prisma.day.create({
             data: {
-                date: new Date('2026-01-04T03:00:00.000'),
+                /** MOnday */
+                date: new Date('2023-01-02T03:00:00.000z'),
                 dayHabits:  {
                     create: {
                         habit_id: firstHabitId,
@@ -95,7 +96,37 @@ async function run() {
             }
         }),
 
+        /**
+         * Habits (Complete/Available): 1/1
+         */
+        prisma.day.create({
+            data:{
+                /** Friday */
+                date: new Date('2023-01-06T03:00:00.000z'),
+                dayHabits: {
+                    create: {
+                        habit_id: firstHabitId,
+                    }
+                }
+            }
+        }),
 
+
+        /**
+         * Habits (Complete/Available): 2/2
+         */
+        prisma.day.create({
+            data: {
+                /** Friday */
+                date: new Date('2023-01-04T03:00:00:000z'),
+                dayHabits: {
+                    create: [
+                        {habit_id: firstHabitId},
+                        {habit_id: secondHabitId},
+                    ]
+                }
+            }
+        }),
     ])
 }
 
